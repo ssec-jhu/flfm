2342b8e9bd7bb98baf4490da73880dde5b92e037
James Noss <jnoss@jhu.edu>
2025-02-13 22:49:15 -0500

    Add test
    Signed-off-by: James Noss <jnoss@jhu.edu>

--- flfm/tests/test_restoration.py
+++ flfm/tests/test_restoration.py
@@ -1,6 +1,12 @@
+import jax.numpy as jnp
+import numpy as np
+
 import flfm.io
 import flfm.restoration
 import flfm.util
+
+CENTER = [1000, 980]
+RADIUS = 230
 
 
 class TestRichardsonLucy:
@@ -9,3 +15,17 @@ class TestRichardsonLucy:
         psf = flfm.io.open(flfm.util.find_repo_location() / "data/yale/measured_psf.tif")
         recon = flfm.restoration.richardson_lucy(image, psf[21])
         assert recon.shape == image.shape
+
+    def test_3d_reconstruction(self):
+        image = flfm.io.open(flfm.util.find_repo_location() / "data/yale/light_field_image.tif")
+        psf = flfm.io.open(flfm.util.find_repo_location() / "data/yale/measured_psf.tif")
+
+        # Normalize PSF.
+        psf /= jnp.sum(psf, axis=(1, 2), keepdims=True)
+
+        recon = flfm.restoration.richardson_lucy(image, psf)
+        assert recon.shape == psf.shape
+        cropped = flfm.util.crop_and_apply_circle_mask(recon, center=CENTER, radius=RADIUS)
+        truth = flfm.io.open(flfm.util.find_repo_location() / "data/yale/reconstructed_image.tif")
+        assert cropped.shape == truth.shape
+        assert np.isclose(truth, cropped).all()
