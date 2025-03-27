9b57c449266a1e738a2ffa9db8a214dd0fdd99e6
Ryan Hausen <ryan.hausen@gmail.com>
2025-03-27 15:13:33 -0400

    moved renamed pytorch files

--- flfm/tests/test_integration.py
+++ flfm/tests/test_integration.py
@@ -5,6 +5,8 @@ from PIL import Image
 
 import flfm.cli
 import flfm.io
+import flfm.pytorch_io
+import flfm.pytorch_restoration
 import flfm.restoration
 import flfm.util
 
@@ -26,7 +28,7 @@ def arr_to_stream(arr: np.ndarray) -> io.BytesIO:
     return stream
 
 
-def test_simple_integration() -> None:
+def test_simple_integration(backend: str) -> None:
     """Test the integration of the package."""
     r = 5
     n, h, w = 4, 32, 32
@@ -52,6 +54,47 @@ def test_simple_integration() -> None:
 
     # read the reconstructed image back in from the byte stream
     out_img = flfm.io.open(out_stream)
+
+    # make a circle mask
+    mask = flfm.util.make_circle_mask(r)
+    out = flfm.util.crop_and_apply_circle_mask(sim_psf, (h // 2, w // 2), r)
+
+    expected_area = np.ceil(np.pi * r**2)
+    assert img.shape == (1, h, w)
+    assert psf.shape == (n, h, w)
+    assert reconstructed.shape == (n, h, w)
+    assert out_img.shape == (n, h, w)
+    assert expected_area == mask.sum()
+    assert mask.shape == (2 * r, 2 * r)
+    assert out.shape == (n, 2 * r, 2 * r)
+
+
+def test_simple_integration_pytorch() -> None:
+    """Test the integration of the package."""
+    r = 5
+    n, h, w = 4, 32, 32
+
+    # setup a random image and psf and save them to a byte stream
+    rand_img = np.ones([1, h, w], dtype=np.float32)
+    rand_img_stream = arr_to_stream(rand_img)
+
+    sim_psf = np.ones([n, h, w], dtype=np.float32) / (w * h)
+    sim_psf_stream = arr_to_stream(sim_psf)
+
+    # read them back in from the byte stream
+    img = flfm.pytorch_io.open(rand_img_stream)
+    psf = flfm.pytorch_io.open(sim_psf_stream)
+
+    # reconstruct the image
+    reconstructed = flfm.pytorch_restoration.richardson_lucy(img, psf, num_iter=1)
+
+    # save the reconstructed image to a byte stream
+    out_stream = io.BytesIO()
+    flfm.pytorch_io.save(out_stream, reconstructed, format="TIFF")
+    out_stream.seek(0)
+
+    # read the reconstructed image back in from the byte stream
+    out_img = flfm.pytorch_io.open(out_stream)
 
     # make a circle mask
     mask = flfm.util.make_circle_mask(r)
--- flfm/tests/test_restoration.py
+++ flfm/tests/test_restoration.py
@@ -1,4 +1,6 @@
 import flfm.io
+import flfm.pytorch_io
+import flfm.pytorch_restoration
 import flfm.restoration
 import flfm.util
 
@@ -8,4 +10,10 @@ class TestRichardsonLucy:
         image = flfm.io.open(flfm.util.find_repo_location() / "data/yale/light_field_image.tif")
         psf = flfm.io.open(flfm.util.find_repo_location() / "data/yale/measured_psf.tif")
         recon = flfm.restoration.richardson_lucy(image, psf[21])
+        assert recon.shape == image.shape
+
+    def test_2d_image_pytorch(self):
+        image = flfm.pytorch_io.open(flfm.util.find_repo_location() / "data/yale/light_field_image.tif")
+        psf = flfm.pytorch_io.open(flfm.util.find_repo_location() / "data/yale/measured_psf.tif")
+        recon = flfm.pytorch_restoration.richardson_lucy(image, psf[21])
         assert recon.shape == image.shape
