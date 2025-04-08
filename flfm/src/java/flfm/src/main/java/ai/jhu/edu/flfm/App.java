e95d35ee93632d59154237507eb04470578d8605
Ryan Hausen <ryan.hausen@gmail.com>
2025-04-08 15:10:11 -0400

    working pytorch export and java run

--- flfm/src/java/flfm/src/main/java/ai/jhu/edu/flfm/App.java
+++ flfm/src/java/flfm/src/main/java/ai/jhu/edu/flfm/App.java
@@ -3,92 +3,123 @@ package ai.jhu.edu.flfm;
 import ai.djl.MalformedModelException;
 import ai.djl.Model;
 import ai.djl.inference.Predictor;
-import ai.djl.modality.cv.BufferedImageFactory;
-import ai.djl.modality.cv.Image;
-import ai.djl.modality.cv.ImageFactory;
 import ai.djl.ndarray.NDArray;
 import ai.djl.ndarray.NDManager;
+import ai.djl.ndarray.index.NDIndex;
+import ai.djl.ndarray.types.Shape;
 import ai.djl.translate.TranslateException;
 
-import java.io.InputStream;
-import java.io.File;
-import java.io.FileInputStream;
 import java.io.IOException;
-import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.Iterator;
-import java.util.List;
-import java.awt.image.BufferedImage;
 
-import javax.imageio.ImageIO;
-import javax.imageio.ImageReader;
-import javax.imageio.stream.ImageInputStream;
-
-import org.apache.commons.imaging.ImageFormats;
-import org.apache.commons.imaging.Imaging;
-
-
-
-
+import ij.ImagePlus;
+import ij.ImageStack;
+import ij.io.FileSaver;
+import ij.io.Opener;
+import ij.process.ImageProcessor;
 
 /**
  * Hello world!
  */
 public class App {
 
-    public static void openArray(String fName) throws Exception {
-        NDManager manager = NDManager.newBaseManager();
-        File tiffFile = new File(fName);
 
-        // Extract individual pages as BufferedImages
-        List<BufferedImage> imagePages = Imaging.getAllBufferedImages(tiffFile);
+    public static NDArray openArrayIJ(String fname, NDManager manager) throws Exception {
+        Opener opener = new Opener();
 
-        for (BufferedImage page : imagePages) {
-            // Convert each page to NDArray
-            Image image = ImageFactory.getInstance().fromImage(page);
-            NDArray ndArray = image.toNDArray(manager);
-            // page.getRGB(x, y) & 0xFFFF; seems to work for 16 bit
-            // Do what this does
-            // https://github.com/imagej/ImageJ/blob/2bfe4ff51a948295cc0a36619a944cf3ee6e7bbc/ij/io/ImageReader.java#L103
+        ImagePlus image = opener.openImage(fname);
 
-            // Do something with the NDArray
-            System.out.println(ndArray.getShape());
-            System.out.println(ndArray.get("1050,952,:"));
+        int width = image.getWidth();
+        int height = image.getHeight();
+        int numSlices = image.getStackSize();
+        System.out.println("Loaded a multipage TIFF with dimensions: " + width + "x" + height + " and " + numSlices + " slices.");
+
+        // Create an NDArray to hold the entire stack
+        NDArray fullStack = manager.zeros(new Shape(numSlices, height, width));
+
+        // Process each slice
+        for (int slice = 1; slice <= numSlices; slice++) {
+            ImageProcessor processor = image.getStack().getProcessor(slice);
+            short[] pixels = (short[]) processor.getPixels();
+
+            // Convert slice to a float array
+            float[] floatPixels = new float[pixels.length];
+            for (int i = 0; i < pixels.length; i++) {
+                floatPixels[i] = pixels[i] & 0xFFFF; // Unsigned conversion
+            }
+
+            // Add slice to the NDArray stack
+            NDArray sliceNDArray = manager.create(floatPixels, new Shape(height, width));
+            fullStack.set(new NDIndex(slice-1+",:,:"), sliceNDArray); // Set the slice into the stack (0-indexed)
         }
+
+        // Print NDArray info
+        System.out.println("NDArray stack shape: " + fullStack.getShape());
+
+        return fullStack;
     }
 
+    public static void saveArrayIJ(String fname, NDArray array) throws Exception {
 
+            // Convert NDArray back to a multipage TIFF
+            ImageStack stack = new ImageStack(
+                (int)array.getShape().get(1),
+                (int)array.getShape().get(2)
+            );
+            int numSlices = (int)array.getShape().get(0);
+
+
+            for (int slice = 0; slice < numSlices; slice++) {
+                // Get each slice from the NDArray (convert to 2D)
+                NDArray sliceNDArray = array.get(slice);
+                float[] slicePixels = sliceNDArray.toFloatArray();
+
+                // Convert float pixels to short (16-bit) array
+                short[] shortPixels = new short[slicePixels.length];
+                for (int i = 0; i < slicePixels.length; i++) {
+                    shortPixels[i] = (short) slicePixels[i];
+                }
+
+                // Add this slice to the ImageStack
+                stack.addSlice(null, shortPixels);
+            }
+
+            // Create an ImagePlus from the stack
+            ImagePlus image = new ImagePlus("Multipage TIFF", stack);
+
+            // Save the image as a multipage TIFF file
+            FileSaver fileSaver = new FileSaver(image);
+            fileSaver.saveAsTiffStack(fname);
+
+            System.out.println("Multipage TIFF saved successfully!");
+    }
 
     public static void main(String[] args) throws Exception, IOException, MalformedModelException, TranslateException  {
-        System.out.println("Hello World!");
-        Path imageFile = Paths.get("/home/ryanhausen/repos/flfm/data/yale/light_field_image.tif");
-        Path psfFile = Paths.get("/home/ryanhausen/repos/flfm/data/yale/measured_psf.tif");
+        String imagePath = "/home/ryanhausen/repos/flfm/data/yale/light_field_image.tif";
+        String psfPath = "/home/ryanhausen/repos/flfm/data/yale/measured_psf.tif";
+        String modelPath = "/home/ryanhausen/repos/flfm/notebooks/richardson_lucy_10.pt";
 
-        openArray("/home/ryanhausen/repos/flfm/data/yale/light_field_image.tif");
-        openArray("/home/ryanhausen/repos/flfm/data/yale/measured_psf.tif");
+        try(NDManager manager = NDManager.newBaseManager()){
+            NDArray image = openArrayIJ(imagePath, manager);
+            System.out.println("Image loaded!");
+            NDArray psf = openArrayIJ(psfPath, manager);
+            NDArray normedPsf = psf.div(psf.sum());
+            System.out.println("PSF loaded!");
 
-        // Open image using Input stream
-        InputStream is = new FileInputStream(imageFile.toFile());
-        Image img = ImageFactory.getInstance().fromInputStream(is);
-        // Image img = ImageFactory.getInstance().fromFile(imageFile);
-        System.out.println("Loaded image!");
-        InputStream psfis = new FileInputStream(psfFile.toFile());
-        Image psf = ImageFactory.getInstance().fromInputStream(psfis);
-        // Image psf = ImageFactory.getInstance().fromFile(psfFile);
-        System.out.println("Loaded psf!");
+            try (Model model = Model.newInstance("flfm", "PyTorch")) {
+                System.out.println("Loading model...");
+                model.load(Paths.get(modelPath));
+                System.out.println("Model loaded!");
 
-        try (Model model = Model.newInstance("flfm", "PyTorch")) {
-            System.out.println("Loading model...");
-            model.load(Paths.get("/home/ryan/repos/flfm/notebooks/richardson_lucy_10.pt"));
-            System.out.println("Model loaded!");
-
-            try (Predictor<Image[], Image[]> predictor = model.newPredictor(new MyTranslator())) {
-                System.out.println("Predicting...");
-                // time the call
-                long start = System.currentTimeMillis();
-                predictor.predict(new Image[]{img, psf});
-                long end = System.currentTimeMillis();
-                System.out.println("Prediction took " + (end - start) + " ms");
+                try (Predictor<NDArray[], NDArray> predictor = model.newPredictor(new MyTranslator())) {
+                    System.out.println("Predicting...");
+                    // time the call
+                    long start = System.currentTimeMillis();
+                    NDArray out = predictor.predict(new NDArray[]{image, normedPsf});
+                    long end = System.currentTimeMillis();
+                    System.out.println("Prediction took " + (end - start) + " ms");
+                    saveArrayIJ("output_file.tiff", out);
+                }
             }
         }
     }
--- flfm/src/java/flfm/src/main/java/ai/jhu/edu/flfm/MyTranslator.java
+++ flfm/src/java/flfm/src/main/java/ai/jhu/edu/flfm/MyTranslator.java
@@ -1,23 +1,21 @@
 package ai.jhu.edu.flfm;
 
-import ai.djl.modality.cv.Image;
-import ai.djl.modality.cv.ImageFactory;
 import ai.djl.ndarray.NDArray;
 import ai.djl.ndarray.NDList;
 import ai.djl.translate.Translator;
 import ai.djl.translate.TranslatorContext;
 
-public class MyTranslator implements Translator<Image[], Image[]> {
+public class MyTranslator implements Translator<NDArray[], NDArray> {
     @Override
-    public Image[] processOutput(TranslatorContext ctx, NDList list) {
+    public NDArray processOutput(TranslatorContext ctx, NDList list) {
         System.out.println("Output shape: " + list.get(0).getShape());
-        return new Image[]{ImageFactory.getInstance().fromNDArray(list.get(0))};
+        return list.get(0);
     }
 
     @Override
-    public NDList processInput(TranslatorContext ctx, Image[] input) {
-        NDArray img = input[0].toNDArray(ctx.getNDManager()).transpose(2, 0, 1);
-        NDArray psf = input[1].toNDArray(ctx.getNDManager()).transpose(2, 0, 1);
+    public NDList processInput(TranslatorContext ctx, NDArray[] input) {
+        NDArray img = input[0];
+        NDArray psf = input[1];
         System.out.println(img.getShape());
         System.out.println(psf.getShape());
 
