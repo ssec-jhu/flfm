ea91bb4c57d093ff12bc5b20f2079538b0a415be
James Noss <jnoss@jhu.edu>
2025-05-08 20:48:36 -0400

    Ammend all paths to data/ -> flfm/tests/data

--- flfm/tests/test_restoration.py
+++ flfm/tests/test_restoration.py
@@ -4,16 +4,20 @@ import flfm.pytorch_restoration
 import flfm.restoration
 import flfm.util
 
+data_dir = flfm.util.find_package_location() / "tests" / "data" / "yale"
+input_filename = data_dir / "light_field_image.tif"
+psf_filename = data_dir / "measured_psf.tif"
+
 
 class TestRichardsonLucy:
     def test_2d_image(self):
-        image = flfm.io.open(flfm.util.find_repo_location() / "data/yale/light_field_image.tif")
-        psf = flfm.io.open(flfm.util.find_repo_location() / "data/yale/measured_psf.tif")
+        image = flfm.io.open(input_filename)
+        psf = flfm.io.open(psf_filename)
         recon = flfm.restoration.richardson_lucy(image, psf[21])
         assert recon.shape == image.shape
 
     def test_2d_image_pytorch(self):
-        image = flfm.pytorch_io.open(flfm.util.find_repo_location() / "data/yale/light_field_image.tif")
-        psf = flfm.pytorch_io.open(flfm.util.find_repo_location() / "data/yale/measured_psf.tif")
+        image = flfm.pytorch_io.open(input_filename)
+        psf = flfm.pytorch_io.open(psf_filename)
         recon = flfm.pytorch_restoration.richardson_lucy(image, psf[21])
         assert recon.shape == image.shape
