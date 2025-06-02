9dc34520844975e3a8ad5d55fe3514fdb2f63cc6
Ryan Hausen <ryan.hausen@gmail.com>
2025-06-02 17:42:11 -0400

    wip perf stats added memray profiler and another requirements file

--- run_perf.sh
+++ run_perf.sh
@@ -5,28 +5,39 @@ rm -f ./perf/torch_cpu.prof
 rm -r ./perf/torch_gpu.prof
 rm -f ./perf/jax_cpu.prof
 rm -r ./perf/jax_gpu.prof
+rm -f ./perf/jax_cpu_mem.prof
+rm -r ./perf/jax_gpu_mem.prof
 
-export CUDA_VISIBLE_DEVICES="0"
-python -m perf.run \
---backend_name "torch" \
---profiler_name "cProfile" \
---out_path "./perf/torch_gpu.prof"
+# export CUDA_VISIBLE_DEVICES="0"
+# python -m perf.run \
+# --backend_name "torch" \
+# --profiler_name "cProfile" \
+# --out_path "./perf/torch_gpu.prof"
 
-python -m perf.run \
---backend_name "jax" \
---profiler_name "cProfile" \
---out_path "./perf/jax_gpu.prof" \
+# python -m perf.run \
+# --backend_name "jax" \
+# --profiler_name "cProfile" \
+# --out_path "./perf/jax_gpu.prof" \
 
 
 export CUDA_VISIBLE_DEVICES=""
 export JAX_PLATFORMS=cpu
-python -m perf.run \
---backend_name "torch" \
---profiler_name "cProfile" \
---out_path "./perf/torch_cpu.prof"
+# python -m perf.run \
+# --backend_name "torch" \
+# --profiler_name "cProfile" \
+# --out_path "./perf/torch_cpu.prof"
+
+# python -m perf.run \
+# --backend_name "jax" \
+# --profiler_name "cProfile" \
+# --out_path "./perf/jax_cpu.prof"
 
 python -m perf.run \
 --backend_name "jax" \
---profiler_name "cProfile" \
---out_path "./perf/jax_cpu.prof"
+--profiler_name "memray" \
+--out_path "./perf/jax_cpu_mem.prof"
 
+python -m perf.run \
+--backend_name "torch" \
+--profiler_name "memray" \
+--out_path "./perf/torch_cpu_mem.prof"
